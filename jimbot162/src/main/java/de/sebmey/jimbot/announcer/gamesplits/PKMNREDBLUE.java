8d799726619890a1c211f87f9378c39ae02b7b35
Vivec <meyer_sebastian@outlook.com>
2018-07-07 05:53:47 +0200

    Game-specific split names and aliases - Pokémon Red #6
    refactored the way aliases are stored into a dynamic array instead of
    harcoding

--- jimbot162/src/main/java/de/sebmey/jimbot/announcer/gamesplits/PKMNREDBLUE.java
+++ jimbot162/src/main/java/de/sebmey/jimbot/announcer/gamesplits/PKMNREDBLUE.java
@@ -25,28 +25,12 @@ public enum PKMNREDBLUE {
 	
 	private String name;
 	private boolean announce;
-	private String alias1;
-	private String alias2;
-	private String alias3;
+	private String[] aliases;
 	
-	private PKMNREDBLUE(String name, boolean announce, String alias1, String alias2, String alias3) {
+	private PKMNREDBLUE(String name, boolean announce, String... aliases) {
 		this.name = name;
 		this.announce = announce;
-		this.alias1 = alias1;
-		this.alias2 = alias2;
-		this.alias3 = alias3;
-	}
-	
-	private PKMNREDBLUE(String name, boolean announce, String alias1, String alias2) {
-		this(name, announce, alias1, alias2, null);
-	}
-	
-	private PKMNREDBLUE(String name, boolean announce, String alias1) {
-		this(name, announce, alias1, null, null);
-	}
-
-	private PKMNREDBLUE(String name, boolean announce) {
-		this(name, announce, null, null, null);
+		this.aliases = aliases;
 	}
 	
 	public static String getSplitNameByNameOrAlias(String search) {
@@ -54,14 +38,10 @@ public enum PKMNREDBLUE {
 			if(split.name != null && split.name.equalsIgnoreCase(search)) {
 				return split.name;
 			}
-			if(split.alias1 != null && split.alias1.equalsIgnoreCase(search)) {
-				return split.name;
-			}
-			if(split.alias2 != null && split.alias2.equalsIgnoreCase(search)) {
-				return split.name;
-			}
-			if(split.alias3 != null && split.alias3.equalsIgnoreCase(search)) {
-				return split.name;
+			for(String alias : split.getAliases()) {
+				if(alias.equalsIgnoreCase(search)) {
+					return split.name;
+				}
 			}
 		}
 		return null;
@@ -72,14 +52,10 @@ public enum PKMNREDBLUE {
 			if(split.name != null && split.name.equalsIgnoreCase(search)) {
 				return split;
 			}
-			if(split.alias1 != null && split.alias1.equalsIgnoreCase(search)) {
-				return split;
-			}
-			if(split.alias2 != null && split.alias2.equalsIgnoreCase(search)) {
-				return split;
-			}
-			if(split.alias3 != null && split.alias3.equalsIgnoreCase(search)) {
-				return split;
+			for(String alias : split.getAliases()) {
+				if(alias.equalsIgnoreCase(search)) {
+					return split;
+				}
 			}
 		}
 		return null;
@@ -93,16 +69,8 @@ public enum PKMNREDBLUE {
 		return announce;
 	}
 
-	public String getAlias1() {
-		return alias1;
-	}
-
-	public String getAlias2() {
-		return alias2;
-	}
-
-	public String getAlias3() {
-		return alias3;
+	public String[] getAliases() {
+		return aliases;
 	}
 	
 }
