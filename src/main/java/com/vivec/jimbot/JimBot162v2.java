45cd4c464d77f63aca0bfe2c6d87e7839bf18c09
JimB16 <f1@jimb.de>
2019-04-28 19:37:54 +0200

    added help message in the case of wrong nr of program arguments

--- src/main/java/com/vivec/jimbot/JimBot162v2.java
+++ src/main/java/com/vivec/jimbot/JimBot162v2.java
@@ -7,6 +7,11 @@ import com.vivec.jimbot.irc.twitch.TwitchJim;
 public class JimBot162v2 {
 
 	public static void main(String[] args) {
+		if(args.length != 3) {
+			System.out.println("usage: jimbot162 BotUsername TwitchOAuth SRLPass");
+			return;
+		}
+
 		CredentialManager man = CredentialManager.getInstance();
 		man.setBotUsername(args[0]);
 		man.setBotTwitchOAuth(args[1]);
