17e56c44e8c804b7e0efea523d2fb8267955fedc
Vivec <meyer_sebastian@outlook.com>
2019-06-02 18:43:40 +0200

    implement method to load all json gamesplit configurations from a given directory

--- src/main/java/com/vivec/jimbot/announcer/gamesplits/GameSplitService.java
+++ src/main/java/com/vivec/jimbot/announcer/gamesplits/GameSplitService.java
@@ -1,20 +1,44 @@
 package com.vivec.jimbot.announcer.gamesplits;
 
 import com.google.gson.Gson;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class GameSplitService {
-
+    private static final Logger LOG = LogManager.getLogger(GameSplitService.class);
     private static Gson gson = new Gson();
 
     private GameSplitService() {}
 
-    static GameSpecificConfiguration loadSingleConfig(Path filePath) throws IOException {
-        byte[] fileContent = Files.readAllBytes(filePath);
-        return gson.fromJson(new String(fileContent), GameSpecificConfiguration.class);
+    static GameSpecificConfiguration loadSingleConfig(Path filePath) {
+        try {
+            byte[] fileContent = Files.readAllBytes(filePath);
+            return gson.fromJson(new String(fileContent), GameSpecificConfiguration.class);
+        } catch (IOException e) {
+            LOG.error("Error while reading from file at path {}", filePath.getFileName(), e);
+            throw new IllegalArgumentException("Error reading from file");
+        }
+
     }
 
+    static List<GameSpecificConfiguration> loadAllConfigsFromDirectory(Path dirPath) {
+        try (Stream<Path> files = Files.walk(dirPath)) {
+            return files
+                    .filter(Files::isRegularFile)
+                    .filter(Files::isReadable)
+                    .filter(p -> p.toString().endsWith(".json"))
+                    .map(GameSplitService::loadSingleConfig)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            LOG.error("Error walking the directory {}", dirPath.getFileName(), e);
+            throw new IllegalArgumentException("Error walking requested directory", e);
+        }
+    }
 }
--- src/test/java/com/vivec/jimbot/announcer/gamesplits/GameSplitServiceTest.java
+++ src/test/java/com/vivec/jimbot/announcer/gamesplits/GameSplitServiceTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,14 +19,22 @@ class GameSplitServiceTest {
     class LoadingFromDisk {
         @Test
         @DisplayName("Load a specific split configuration from disk")
-        void load_pkmn_red_blue() throws IOException, URISyntaxException {
+        void load_pkmn_red_blue() throws URISyntaxException {
             final String filename = "6_PKMNREDBLUE.json";
-            GameSpecificConfiguration singleConfig = GameSplitService.loadSingleConfig(Paths.get(Objects.requireNonNull(GameSplitService.class.getClassLoader().getResource("gamesplits/" + filename)).toURI()));
+            Path filePath = Paths.get(Objects.requireNonNull(GameSplitService.class.getClassLoader().getResource("gamesplits/" + filename)).toURI());
+            GameSpecificConfiguration singleConfig = GameSplitService.loadSingleConfig(filePath);
             assertThat(singleConfig).isNotNull();
             assertThat(singleConfig.getGameId()).isEqualTo(6);
             assertThat(singleConfig.getCategoryName()).isEqualTo("Any% Glitchless");
             assertThat(singleConfig.getSplits()).hasSize(1);
         }
-    }
 
+        @Test
+        void load_all_config_files_from_directory() throws URISyntaxException {
+            Path filePath = Paths.get(Objects.requireNonNull(GameSplitService.class.getClassLoader().getResource("test_gamesplits/")).toURI());
+            List<GameSpecificConfiguration> allTestConfigs = GameSplitService.loadAllConfigsFromDirectory(filePath);
+            assertThat(allTestConfigs).isNotNull();
+            assertThat(allTestConfigs).hasSize(2);
+        }
+    }
 }
